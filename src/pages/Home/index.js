3894320464198f8c91988bca8697a008d7b251ea
Jeef-Jr <jefferson.goncalvesfo@sptech.school>
2022-06-15 20:22:25 -0300

    Mudança de Layout e criação de Actions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,97 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, ScrollView, StyleSheet } from "react-native";
+import Actions from "../../components/Actions";
 import Balance from "../../components/Balance";
 import Header from "../../components/Header";
 
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faBarcode,
+  faMoneyBillTransfer,
+  faCommentDollar,
+  faDollarSign,
+  faMobile,
+  faHeart,
+  faGlobe,
+} from "@fortawesome/free-solid-svg-icons";
+
 export default function Home() {
   return (
-    <View style={styles.container}>
+    <View>
       <Header />
       <Balance />
+      <View style={styles.container}>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <Actions
+            name="Pagar"
+            icon={
+              <FontAwesomeIcon
+                style={styles.icons}
+                size={25}
+                icon={faBarcode}
+              />
+            }
+          />
+          <Actions
+            name="Trânferir"
+            icon={
+              <FontAwesomeIcon
+                style={styles.icons}
+                size={25}
+                icon={faMoneyBillTransfer}
+              />
+            }
+          />
+          <Actions
+            name="Depositar"
+            icon={
+              <FontAwesomeIcon
+                style={styles.icons}
+                size={25}
+                icon={faDollarSign}
+              />
+            }
+          />
+          <Actions
+            name="Cobrar"
+            icon={
+              <FontAwesomeIcon
+                style={styles.icons}
+                size={25}
+                icon={faCommentDollar}
+              />
+            }
+          />
+          <Actions
+            name="Recargar"
+            icon={
+              <FontAwesomeIcon style={styles.icons} size={25} icon={faMobile} />
+            }
+          />
+          <Actions
+            name="Doação"
+            icon={
+              <FontAwesomeIcon style={styles.icons} size={25} icon={faHeart} />
+            }
+          />
+          <Actions
+            name="Trâns. Inter"
+            icon={
+              <FontAwesomeIcon style={styles.icons} size={25} icon={faGlobe} />
+            }
+          />
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fafafa",
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+  icons: {
+    color: "#fff",
   },
 });
